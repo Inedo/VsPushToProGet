890008545128c6e548312ec22aa62bb6193e5155
Ben Lubar <ben.lubar@gmail.com>
2017-03-10 10:08:03 -0600

    Support Visual Studio Pro and Visual Studio Enterprise

--- PushToProGet/source.extension.cs
+++ PushToProGet/source.extension.cs
@@ -11,7 +11,7 @@ namespace PushToProGet
         public const string Name = "Push to ProGet";
         public const string Description = @"A Visual Studio Extension that allows you to automatically package a project and push it to a ProGet instance";
         public const string Language = "en-US";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
         public const string Author = "Inedo";
         public const string Tags = "Package Manager, Inedo, ProGet, .NET, Visual Studio 2012, Visual Studio 2013, Visual Studio 2015, Visual Studio 2017, Productivity, Repository";
     }
